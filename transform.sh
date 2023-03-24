7b7614f515a90f0706dbe6d7a0b6dffee94a10ae
amolari <alexandre.molari@wirdgroup.com>
2023-03-24 17:11:39 +0100

    Delete transform.sh

--- transform.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export instanceId=`curl http://169.254.169.254/latest/meta-data/instance-id`
-source /config/cloud/aws/onboard_config_vars
-export $(cut -s -d= -f1 /config/cloud/aws/onboard_config_vars)
-
-envsubst < telemetry-test.json > my-telemetry-test.json
